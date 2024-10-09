899c0eebd21a67c0d0417361dfe23a9c0a23645c
Tsvetkov.Andrey <saintsandrei@gmail.com>
2024-10-09 11:59:12 +0300

    Инициализация проекта

---
+++ src/test/java/com/example/walletproject/WalletProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.walletproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
