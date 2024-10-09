4824cc511a09418cba3ebb7cb79811a8ddf95228
Tsvetkov.Andrey <saintsandrei@gmail.com>
2024-10-09 17:12:07 +0300

    Создание класса Role

---
+++ src/main/java/com/example/walletproject/entity/Role.java
@@ -0,0 +1,29 @@
+package com.example.walletproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "t_role")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role implements GrantedAuthority {
+
+    @Id
+    private int id;
+    private String name;
+    @Transient
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    @Override
+    public String getAuthority() {
+        return getName();
+    }
+}
--- src/main/java/com/example/walletproject/entity/User.java
+++ src/main/java/com/example/walletproject/entity/User.java
@@ -1,12 +1,22 @@
 package com.example.walletproject.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 
 @Data
 @Entity
 @Table(name = "t_users")
-public class User {
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -14,10 +24,41 @@ public class User {
 
     @Column(unique=true, nullable=false, length=20, updatable=false)
     private String username;
+
     @Column(nullable = false, length=20)
     private String password;
+
     @Column(nullable=false, length=50)
     private String email;
+
     @Column(nullable=false, length=11)
     private String phone;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Role> roles;
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
 }
