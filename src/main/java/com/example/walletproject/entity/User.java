9be9fdb8a28df212d885e19fd68f8cdf058b987b
Tsvetkov.Andrey <saintsandrei@gmail.com>
2024-10-09 12:17:49 +0300

    Создание сущности User

---
+++ src/main/java/com/example/walletproject/entity/User.java
@@ -0,0 +1,23 @@
+package com.example.walletproject.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(unique=true, nullable=false, length=20, updatable=false)
+    private String username;
+    @Column(nullable = false, length=20)
+    private String password;
+    @Column(nullable=false, length=50)
+    private String email;
+    @Column(nullable=false, length=11)
+    private String phone;
+}
